028b2ea3a4dc3800ef44da5b757a972ba7443d4e
Conrad Taylor <conradwt@gmail.com>
2021-06-20 18:43:23 -0700

    Add root route.

--- lib/liveview_app_web/router.ex
+++ lib/liveview_app_web/router.ex
@@ -17,7 +17,7 @@ defmodule LiveviewAppWeb.Router do
   scope "/", LiveviewAppWeb do
     pipe_through :browser
 
-    live "/", PageLive, :index
+    live "/", UserLive.Index, :index
 
     live "/users", UserLive.Index, :index
     live "/users/new", UserLive.Index, :new
